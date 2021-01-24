d4525878a4547b5b95cbbbe6301a19257c7a3b03
virunpatch <guyver125@hotmail.com>
2021-01-24 19:25:34 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'favoriteA.dart';
+import 'favoriteB.dart';
+import 'favoriteC.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,20 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sett The Boss',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Sett The Boss'),
     );
@@ -33,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,86 +36,92 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /*1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /*2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Sett The Boss',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Game,MOBA',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        /*3*/
+        FavoriteWidget(),
+      ],
+    ),
+  );
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'A leader of Ionia’s growing criminal underworld, Sett rose to prominence in the wake of the war with Noxus.'
+      'Though he began as a humble challenger in the fighting pits of Navori, he quickly gained notoriety for his savage strength, and his ability to take seemingly endless amounts of punishment.'
+      ' Now, having climbed through the ranks of local combatants, Sett has muscled to the top, reigning over the pits he once fought in.',
+      softWrap: true,
+    ),
+  );
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     Color color = Theme.of(context).primaryColor;
 
     Widget buttonSection = Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TapboxA()),
+                );
+              },
+              child: _buildButtonColumn(color, Icons.home_rounded, 'หน้าหลัก')),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ParentWidget()),
+                );
+              },
+              child: _buildButtonColumn(color, Icons.favorite, 'ถูกใจ')),
+          GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ParentWidgetC()),
+                );
+              },
+              child: _buildButtonColumn(color, Icons.share, 'แบ่งปัน')),
         ],
       ),
     );
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    '“Im undisputed.”',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'A leader of Ionia’s growing criminal underworld, Sett rose to prominence in the wake of the war with Noxus.'
-        'Though he began as a humble challenger in the fighting pits of Navori, he quickly gained notoriety for his savage strength, and his ability to take seemingly endless amounts of punishment.'
-        ' Now, having climbed through the ranks of local combatants, Sett has muscled to the top, reigning over the pits he once fought in.',
-        softWrap: true,
-      ),
-    );
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Sett The Boss'),
       ),
       body: ListView(
         children: <Widget>[
@@ -179,21 +167,15 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-// #docregion FavoriteWidget
 class FavoriteWidget extends StatefulWidget {
   @override
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
 }
-// #enddocregion FavoriteWidget
 
-// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
 class _FavoriteWidgetState extends State<FavoriteWidget> {
-  // #enddocregion _FavoriteWidgetState-build
   bool _isFavorited = true;
-  int _favoriteCount = 41;
-  // #enddocregion _FavoriteWidgetState-fields
+  int _favoriteCount = 80;
 
-  // #docregion _toggleFavorite
   void _toggleFavorite() {
     setState(() {
       if (_isFavorited) {
@@ -205,10 +187,7 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
       }
     });
   }
-  // #enddocregion _toggleFavorite
 
-  // #docregion _FavoriteWidgetState-build
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
@@ -232,5 +211,4 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
       ],
     );
   }
-  // #docregion _FavoriteWidgetState-fields
 }
